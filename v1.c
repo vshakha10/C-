e7915137abd575a8fbc025350fb242b8b6adbe12
nirali8 <nirali.skillqode2gmail.com>
2024-05-21 08:32:52 +0530

    change

--- v1.c
+++ v1.c
@@ -1,8 +1,11 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 int main()
 {
     float r = 9;
-    
+    cout << 3.14 * r * r;
+    cout << "area is :" << endl;
+    cout << a << endl;
+    return 0;
 }
